ce01a855329fd4c712df867e4285ad680db6c07d
matheo-prd <perod.matheo@gmail.com>
2025-07-13 13:29:32 +0200

    feat(#39): ajout du composant PlayerList affichant la liste des joueurs d’un serveur
    - Création du composant Astro `PlayerList`
    - Récupération des joueurs via l'API publique :
      https://otterlyapi.antredesloutres.fr/api/joueurs/stats-serveur/stats-by-server/{serveurId}
    - Affichage en grille responsive (CSS Grid)
    - Affichage du nom de chaque joueur et de son avatar Minecraft (via mc-heads.net)
    - Gestion du fallback si `json.data` est vide ou non défini
    - Ajout de styles CSS :
      - Cartes avec avatar, nom, effet hover
      - Mise en colonnes responsive avec `grid-template-columns`
      - Augmentation de la largeur des vignettes (min 180px)

---
+++ src/components/serveurs/ServeurType.ts
@@ -0,0 +1,15 @@
+export type ServeurType = {
+    nom : string,
+    jeu : string,
+    version: string,
+    modpack: string,
+    modpack_url: string,
+    nom_monde: string,
+    embed_color: string,
+    actif: boolean,
+    global: boolean
+    image: string,
+    description: string,
+    contenaire: string,
+    type: string
+}
