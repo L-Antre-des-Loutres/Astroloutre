c50fd9a2177995d39dd977ce9453db3a3907dc9c
matheo-1712 <perod.matheo@gmail.com>
2025-09-16 13:46:09 +0200

    Ajustement du calcul du score d'activité dans `discordActivityScore` :
    - Modification de la pondération des messages : 1 message vaut désormais 0,75 point au lieu de 1 point.
    - Refactorisation de la méthode de calcul pour inclure une nouvelle formule prenant en compte un coefficient de saturation vocal (`saturationVoc`) affectant les points basés sur le temps passé en vocal.
    - Amélioration de la robustesse du code avec des vérifications explicites des paramètres `nbMessage` et `tmpsVocal` pour éviter les valeurs négatives ou indéfinies.
    
    Signed-off-by: matheo-1712 <perod.matheo@gmail.com>

--- src/utils/discordActivityScore.ts
+++ src/utils/discordActivityScore.ts
@@ -1,29 +1,29 @@
 /**
  * Calculates a Discord activity score based on the number of messages sent and the time spent in voice activity.
  *
- * 1 message = 1 point
+ * 1 message = 0.75 point
  *
- * 1 minute de vocal = 1 point
+ * vocal calcul à revoir !
  *
  * @param {number} nbMessage - The number of messages sent by the user. If less than or equal to 0, it is treated as 0.
  * @param {number} tmpsVocal - The time spent in voice channels by the user. If less than or equal to 0, it is treated as 0.
  * @return {Promise<number>} A promise that resolves to the total activity score calculated by summing the points for messages and voice time.
  */
 
-
 export async function discordActivityScore(nbMessage: number, tmpsVocal: number): Promise<number> {
+    let saturationVoc = 0.1;
     let nbPoint = 0;
 
-    // Formule de point pour les messages
-    if (nbMessage > 0) {
-        nbPoint += nbMessage;
+    if (!nbMessage) {
+        nbMessage = 0;
     }
 
-    // Formule de point pour les temps vocaux
-    if (tmpsVocal > 0) {
-        // Convertit les minutes en points
-        nbPoint += Math.floor(tmpsVocal / 60);
+    if (!tmpsVocal) {
+        tmpsVocal = 0;
     }
+
+    // Formule de point pour les messages
+    nbPoint = (nbMessage * 0.75) + (10 * (saturationVoc * tmpsVocal))
 
     return nbPoint;
 }
