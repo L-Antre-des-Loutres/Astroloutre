d2657e02c3acf316c0ed18bbd9781f8959777ca8
matheo-1712 <perod.matheo@gmail.com>
2025-10-03 11:40:25 +0200

    Ajout d'une gestion des exceptions et formes personnalisées pour les sprites Pokémon dans `showdownApiGetPkmArt.ts` :
    - Ajout d'une gestion des erreurs avec un retour vide si `pokemonName` est indéfini.
    - Support des formes personnalisées avec un chemin dédié (`/pokemon/rlm/forme/...`) pour les formes spéciales.
    - Création des fichiers `probopass.webp` et `nosepass.webp` dans `public/pokemon/rlm/forme`.
    - Garantit une gestion plus flexible et robuste des sprites Pokémon, avec une meilleure prise en charge des cas spécifiques.
    
    Signed-off-by: matheo-1712 <perod.matheo@gmail.com>

--- src/utils/showdownApiGetPkmArt.ts
+++ src/utils/showdownApiGetPkmArt.ts
@@ -2,13 +2,25 @@ const apiUrl = "https://play.pokemonshowdown.com/sprites/home/"
 const apiUrlShiny = "https://play.pokemonshowdown.com/sprites/home-shiny/"
 const defaultPokemonArt = "https://play.pokemonshowdown.com/sprites/substitutes/gen5/substitute.png"
 
-export async function showdownApiGetPkmArt(pokemonName: string, forme: string = "normal", shiny: boolean = false) {
+export async function showdownApiGetPkmArt(pokemonName: string, forme: string = "normal", shiny: boolean = false): Promise<string> {
 
     // Préparation de la variable pokemonArt
     let pokemonArt: string
+
+    // Gestion des erreurs
+    // Si pokemonName est vide
+    if (!pokemonName) {
+        return ""
+    }
+
+    // Gestion des exceptions
     if (pokemonName.toLowerCase() === "flammiko") {
         return pokemonArt = "/pokemon/rlm/flammiko.webp"
     }
+    if (forme.toLowerCase() === "rlm") {
+        return pokemonArt = "/pokemon/rlm/forme/" + pokemonName.toLowerCase() + ".webp"
+    }
+    // FIN de gestion des exceptions
 
     if (shiny) {
         pokemonArt = `${apiUrlShiny}${pokemonName}.png`
