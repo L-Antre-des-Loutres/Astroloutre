005801f6a5efdf15fdc48f03ab5bfb6840d4479b
matheo-1712 <perod.matheo@gmail.com>
2025-10-03 20:12:27 +0200

    Suppression de la documentation pour le bot "Citlali" et mise à jour des dépendances `astro` et autres paquets vers leurs versions les plus récentes.
    Signed-off-by: matheo-1712 <perod.matheo@gmail.com>

--- src/content.config.ts
+++ src/content.config.ts
@@ -9,7 +9,7 @@ const blog = defineCollection({
         pubDate: z.coerce.date(),
         tags: z.array(z.string()),
         author: z.string(),
-        image: z.string().optional(),
+        image: z.string().optional()
     })
 });
 
