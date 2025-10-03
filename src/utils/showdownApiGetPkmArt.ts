6ed7d4135ba06a82261e8b37328fa5329d67d447
matheo-1712 <perod.matheo@gmail.com>
2025-10-03 10:41:28 +0200

    Ajout d'une gestion des sprites Pokémon avec la fonctionnalité shiny dans `PokemonTeams.astro` et `showdownApiGetPkmArt.ts` :
    - Création du fichier `showdownApiGetPkmArt.ts` pour récupérer les sprites Pokémon depuis l'API de Pokémon Showdown :
      - Gestion des sprites normaux et shiny grâce à une condition basée sur un booléen.
      - Ajout d'une image par défaut lorsque le sprite du Pokémon est introuvable.
    - Mise à jour de `PokemonTeams.astro` :
      - Ajout d'un paramètre pour indiquer si un Pokémon est shiny via l'attribut `shiny`.
      - Configuration des sprites Pokémon dans une grille en utilisant la nouvelle fonction `showdownApiGetPkmArt`.
    - Ajout d'un affichage alternatif textuel dans la page Minecraft : intégration d'un titre listant les Pokémon d'une équipe.
    
    Amélioration de la personnalisation et de la qualité visuelle dans la gestion des équipes Pokémon.
    
    Signed-off-by: matheo-1712 <perod.matheo@gmail.com>

---
+++ src/utils/showdownApiGetPkmArt.ts
@@ -0,0 +1,27 @@
+const apiUrl = "https://play.pokemonshowdown.com/sprites/home/"
+const apiUrlShiny = "https://play.pokemonshowdown.com/sprites/home-shiny/"
+const defaultPokemonArt = "https://play.pokemonshowdown.com/sprites/substitutes/gen5/substitute.png"
+
+export async function showdownApiGetPkmArt(pokemonName: string, forme: string = "normal", shiny: boolean = false) {
+
+    // Préparation de la variable pokemonArt
+    let pokemonArt: string
+
+    if (shiny) {
+        pokemonArt = `${apiUrlShiny}${pokemonName}.png`
+    } else {
+        pokemonArt = `${apiUrl}${pokemonName}.png`
+    }
+
+    // On vérifie que l'image existe
+    return fetch(pokemonArt).then(res => {
+        if (res.ok) {
+            return pokemonArt
+        } else {
+            // On renvoie une image de clone
+            return defaultPokemonArt
+        }
+    }).catch(() => {
+        return defaultPokemonArt
+    })
+}
