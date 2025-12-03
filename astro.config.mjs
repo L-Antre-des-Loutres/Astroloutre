fe56fe6d159edc335d4cf8b591f1c7a5409e203c
matheo-1712 <perod.matheo@gmail.com>
2025-12-03 14:53:53 +0100

    Add robots.txt support, integrate astro-robots-txt package, update astro
    Signed-off-by: matheo-1712 <perod.matheo@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,12 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import mdx from "@astrojs/mdx";
 
+import robotsTxt from 'astro-robots-txt';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://antredesloutres.fr',
-    integrations: [react(), sitemap(), mdx()],
+    integrations: [react(), sitemap(), mdx(), robotsTxt()],
 
   vite: {
     plugins: [tailwindcss()]
