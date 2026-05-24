488a13c8e7d0976a5c9d970c9a2016ac8aaaf42e
matheo-1712 <perod.matheo@gmail.com>
2026-05-24 18:30:05 +0200

    #update : Fix username page
    Signed-off-by: matheo-1712 <perod.matheo@gmail.com>

--- src/utils/constantes.ts
+++ src/utils/constantes.ts
@@ -2,3 +2,10 @@
 //  API URL
 // ──────────────────────────────────────────────
 export const PB_URL = "https://pocketbase.antredesloutres.fr"
+
+
+// ──────────────────────────────────────────────
+//  Collections names
+// ──────────────────────────────────────────────
+export const DISCORD_USERS = "discord_users"
+export const DISCORD_USER_STATS = "discord_user_stats"
